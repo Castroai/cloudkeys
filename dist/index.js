cb3426294bc1b524d1d27487c734b11c99c15d61
Steven Castro <castrostevenyc@gmail.com>
2023-08-23 21:02:41 -0400

    update

--- dist/index.js
+++ dist/index.js
@@ -9,13 +9,35 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const identity_1 = require("@azure/identity");
+const keyvault_secrets_1 = require("@azure/keyvault-secrets");
+// import keyVaultConfig from "../cloudkeys-config.json";
+const fs_1 = require("fs");
 const figlet_1 = __importDefault(require("figlet"));
 const commander_1 = require("commander");
-const fs_1 = __importDefault(require("fs"));
+const keyVaultConfig = {
+    keyVaultUrl: "https://keyvault-castroai.vault.azure.net/",
+};
+// A Util to check if json file contains all the keys
+const hasAllKeys = (obj, keys) => {
+    for (const key of keys) {
+        if (!(key in obj)) {
+            return false;
+        }
+    }
+    return true;
+};
 console.log(figlet_1.default.textSync("CLOUD KEYS"));
 const program = new commander_1.Command();
 program
@@ -24,24 +46,43 @@ program
     .option("-g, --generate", "Generate .env file")
     .parse(process.argv);
 function GenerateEnvFile() {
+    var _a, e_1, _b, _c;
     return __awaiter(this, void 0, void 0, function* () {
-        const filePath = "./.env";
-        const content = "This is the content of the new file.";
-        try {
-            fs_1.default.writeFile(filePath, content, (err) => {
-                if (err) {
-                    console.error("Error creating the file:", err);
+        // List of keys to check for in the json file
+        const keysToCheck = ["keyVaultUrl"];
+        const hasAll = hasAllKeys(keyVaultConfig, keysToCheck);
+        if (hasAll) {
+            const envData = [];
+            const keyVaultUrl = keyVaultConfig.keyVaultUrl;
+            const credential = new identity_1.DefaultAzureCredential();
+            const client = new keyvault_secrets_1.SecretClient(keyVaultUrl, credential);
+            try {
+                for (var _d = true, _e = __asyncValues(client.listPropertiesOfSecrets()), _f; _f = yield _e.next(), _a = _f.done, !_a; _d = true) {
+                    _c = _f.value;
+                    _d = false;
+                    const secretProperties = _c;
+                    const secretName = secretProperties.name;
+                    const secret = yield client.getSecret(secretName);
+                    const secretValue = secret.value;
+                    envData.push(`${secretName}=${secretValue}`);
                 }
-                else {
-                    console.log("File created successfully!");
+            }
+            catch (e_1_1) { e_1 = { error: e_1_1 }; }
+            finally {
+                try {
+                    if (!_d && !_a && (_b = _e.return)) yield _b.call(_e);
                 }
-            });
+                finally { if (e_1) throw e_1.error; }
+            }
+            (0, fs_1.writeFileSync)(".env", envData.join("\n"));
+            console.log("Secrets exported to .env file.");
         }
-        catch (error) { }
+        else {
+            throw new Error("Json not complete");
+        }
     });
 }
 const options = program.opts();
 if (options.generate) {
     GenerateEnvFile();
 }
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+#! /usr/bin/env node
 import { DefaultAzureCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
-import keyVaultConfig from "../cloudkeys-config.json";
+// import keyVaultConfig from "../cloudkeys-config.json";
 import { writeFileSync } from "fs";
+import figlet from "figlet";
+import { Command } from "commander";
+let keyVaultConfig = {
+  keyVaultUrl: "https://keyvault-castroai.vault.azure.net/",
+};
 
 // A Util to check if json file contains all the keys
 const hasAllKeys = (obj: Record<string, any>, keys: string[]): boolean => {
@@ -12,15 +18,26 @@ const hasAllKeys = (obj: Record<string, any>, keys: string[]): boolean => {
   }
   return true;
 };
-// List of keys to check for in the json file
-const keysToCheck = ["keyVaultUrl"];
-const hasAll = hasAllKeys(keyVaultConfig, keysToCheck);
-if (hasAll) {
-  const envData = [];
-  const keyVaultUrl = keyVaultConfig.keyVaultUrl;
-  const credential = new DefaultAzureCredential();
-  const client = new SecretClient(keyVaultUrl, credential);
-  async function main() {
+
+import fs from "fs";
+console.log(figlet.textSync("CLOUD KEYS"));
+const program = new Command();
+program
+  .version("1.0.0")
+  .description("A CLI to download env variables")
+  .option("-g, --generate", "Generate .env file")
+  .parse(process.argv);
+
+async function GenerateEnvFile() {
+  // List of keys to check for in the json file
+
+  const keysToCheck = ["keyVaultUrl"];
+  const hasAll = hasAllKeys(keyVaultConfig, keysToCheck);
+  if (hasAll) {
+    const envData = [];
+    const keyVaultUrl = keyVaultConfig.keyVaultUrl;
+    const credential = new DefaultAzureCredential();
+    const client = new SecretClient(keyVaultUrl, credential);
     for await (const secretProperties of client.listPropertiesOfSecrets()) {
       const secretName = secretProperties.name;
       const secret = await client.getSecret(secretName);
@@ -29,8 +46,12 @@ if (hasAll) {
     }
     writeFileSync(".env", envData.join("\n"));
     console.log("Secrets exported to .env file.");
+  } else {
+    throw new Error("Json not complete");
   }
-  main();
-} else {
-  throw new Error("Json not complete");
+}
+
+const options = program.opts();
+if (options.generate) {
+  GenerateEnvFile();
 }
